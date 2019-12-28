9b4e21269bb108aeebd4f2ac9eee6c6d63b61c93
Luis G. Rivera <luis.rivera162@upr.edu>
2019-12-28 16:30:13 -0500

    Added my longestPalindrome solution

---
+++ String_and_Arrays/longestPalindrome.py
@@ -0,0 +1,38 @@
+
+# https://leetcode.com/problems/longest-palindromic-substring/
+
+
+class Solution(object):
+
+    def longest_palindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        if len(s) == 1:
+            return s
+
+        result_string = ""
+
+        for index in range(len(s) - 1):
+            even = self.helper(s, index, index)
+            odd = self.helper(s, index, index + 1)
+
+            if len(even) > len(result_string):
+                result_string = even
+
+            if len(odd) > len(result_string):
+                result_string = odd
+
+        return result_string
+
+    def helper(self, s, left, right):
+
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left = left - 1
+            right = right + 1
+
+        return s[left + 1:right]
+
+#inspired by https://leetcode.com/problems/longest-palindromic-substring/discuss/461877/Cleanest-Python-O(n2)-Time-O(1)-Space-Solution
