fb779e088c2db329ff2eed9ce558b59c8579c0f6
Luis G. Rivera <luis.rivera162@upr.edu>
2019-12-31 21:20:31 -0500

    Added my ContainerWithMostWater solution

---
+++ String_and_Arrays/ContainerWithMostWater.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/container-with-most-water/
+
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        max_area = 0
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            area = (right - left) * min(height[left], height[right])
+            max_area = max(max_area, area)
+
+            if height[left] < height[right]:
+                left += 1
+
+            else:
+                right -= 1
+
+        return max_area
