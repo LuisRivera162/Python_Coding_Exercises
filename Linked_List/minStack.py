35931e37c4a2cb82947a1c9df9db3425647a78b6
Luis G. Rivera <luis.rivera162@upr.edu>
2020-01-04 13:38:11 -0500

    Added my minStack solution

--- Linked_List/minStack.py
+++ Linked_List/minStack.py
@@ -14,35 +14,35 @@ class MinStack:
         initialize your data structure here.
         """
         self.size = 0
-        self.dummy_head = ListNode(0, None, None)
-        self.dummy_min = ListNode(0, None, None)
+        self.head = ListNode(0, None, None)
+        self.min_head = ListNode(0, None, None)
 
     def push(self, x: int) -> None:
-        if self.size == 0 or x <= self.dummy_min.val:
-            self.dummy_min.next = ListNode(x, None, self.dummy_min)
-            self.dummy_min = self.dummy_min.next
+        if self.size == 0 or x <= self.min_head.val:
+            self.min_head.next = ListNode(x, None, self.min_head)
+            self.min_head = self.min_head.next
 
-        self.dummy_head.next = ListNode(x, None, self.dummy_head)
-        self.dummy_head = self.dummy_head.next
+        self.head.next = ListNode(x, None, self.head)
+        self.head = self.head.next
         self.size += 1
 
     def pop(self) -> None:
         if self.size == 0:
             return
 
-        if self.dummy_head.val == self.dummy_min.val:
-            self.dummy_min = self.dummy_min.prev
-            self.dummy_min.next = None
+        if self.head.val == self.min_head.val:
+            self.min_head = self.min_head.prev
+            self.min_head.next = None
 
-        self.dummy_head = self.dummy_head.prev
-        self.dummy_head.next = None
+        self.head = self.head.prev
+        self.head.next = None
         self.size -= 1
 
     def top(self) -> int:
-        return self.dummy_head.val
+        return self.head.val
 
     def getMin(self) -> int:
-        return self.dummy_min.val
+        return self.min_head.val
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
