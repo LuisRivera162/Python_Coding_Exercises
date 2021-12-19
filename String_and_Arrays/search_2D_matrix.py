1ad431098cc7d8a503019feab736b743e8429ab2
luisRivera162 <luis.rivera162@upr.edu>
2021-12-19 00:32:36 -0400

    Updated solution to use binary search in rows as well.

--- String_and_Arrays/search_2D_matrix.py
+++ String_and_Arrays/search_2D_matrix.py
@@ -2,9 +2,20 @@
 
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-        for i in range(len(matrix)):
-            if target <= matrix[i][-1]:
-                return self.binary_search(matrix[i], target)
+        left = 0
+        right = len(matrix)-1
+        
+        while left <= right:
+            mid = int((left+right)/2)
+            
+            if matrix[mid][-1] == target or matrix[mid][0] == target:
+                return True
+            elif matrix[mid][-1] < target:
+                left = mid + 1
+            elif matrix[mid][0] > target:
+                right = mid - 1
+            else:
+                return self.binary_search(matrix[mid], target)
         return False
     
     def binary_search(self, list, target):
